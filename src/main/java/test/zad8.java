e42fbe4c3c2d7ad8690f5178169dd014ee514d18
Mike <jablonowski.michal@wp.pl>
2020-02-22 14:51:10 +0100

    zad4

---
+++ src/main/java/test/zad8.java
@@ -0,0 +1,31 @@
+package test;
+
+public class zad8 {
+
+    public static void repleaceStos(int[] arr) {
+        int n = arr.length - 1;
+        int a = 0;
+        for (int i = 0; i <= n; i++) {
+            if (arr[i] > 0) {
+                a = a + arr[i];
+
+                System.out.print(arr[i] + "; ");
+            }
+        }
+        System.out.println();
+        System.out.println("Najwieksza suma wybranych liczb z powyższego zbioru wynosi:" + a);
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {-1, 2, 3, 4, -2};
+        for (int x = 0; x <= arr.length - 1; x++) {
+            System.out.print(arr[x] + "; ");
+        }
+        System.out.println();
+        repleaceStos(arr);
+
+    }
+
+
+}
